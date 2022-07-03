5919e46b4c5c4cfd77150da2848d26e7f9387b68
ShinJaehun <gunsmoke.shin@gmail.com>
2022-07-03 14:29:02 +0900

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,265 @@
+/*
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  //let squares = Array.from(document.querySelectorAll(".grid div"));
+  let squares = Array.from(document.querySelectorAll(".grid div"));
+  const width = 10;
+
+  console.log("hello");
+  console.log(squares);
+});
+*/
+
+let squares = Array.from(document.querySelectorAll(".grid div"));
+//console.log(squares);
+
+function showAlert() {
+  alert("you have been alerted");
+}
+
+const grid = document.querySelector(".grid");
+const scoreDisplay = document.querySelector("#score");
+const startBtn = document.querySelector("#start-button");
+
+const width = 10;
+let nextRandom = 0;
+let timerId;
+let score = 0;
+const colors = ["orange", "red", "purple", "green", "blue"];
+
+//The Tetrominoes
+const lTetromino = [
+  [1, width + 1, width * 2 + 1, 2],
+  [width, width + 1, width + 2, width * 2 + 2],
+  [1, width + 1, width * 2 + 1, width * 2],
+  [width, width * 2, width * 2 + 1, width * 2 + 2]
+];
+
+const zTetromino = [
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1]
+];
+
+const tTetromino = [
+  [1, width, width + 1, width + 2],
+  [1, width + 1, width + 2, width * 2 + 1],
+  [width, width + 1, width + 2, width * 2 + 1],
+  [1, width, width + 1, width * 2 + 1]
+];
+
+const oTetromino = [
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1]
+];
+
+const iTetromino = [
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3]
+];
+
+const theTetrominoes = [
+  lTetromino,
+  zTetromino,
+  tTetromino,
+  oTetromino,
+  iTetromino
+];
+
+let currentPosition = 4;
+let currentRotation = 0;
+
+let random = Math.floor(Math.random() * theTetrominoes.length);
+//console.log(random);
+let current = theTetrominoes[random][currentRotation];
+
+function draw() {
+  current.forEach((index) => {
+    squares[currentPosition + index].classList.add("tetromino");
+    squares[currentPosition + index].style.backgroundColor = colors[random];
+  });
+}
+
+function undraw() {
+  current.forEach((index) => {
+    squares[currentPosition + index].classList.remove("tetromino");
+    squares[currentPosition + index].style.backgroundColor = "";
+  });
+}
+
+//timerId = setInterval(moveDown, 1000);
+// startBtn으로 Interval을 조정하게 만들면서 필요 없게 됨
+
+function control(e) {
+  if (e.keyCode === 37) {
+    moveLeft();
+  } else if (e.keyCode === 38) {
+    rotate();
+  } else if (e.keyCode === 39) {
+    moveRight();
+  } else if (e.keyCode === 40) {
+    moveDown();
+  }
+}
+
+document.addEventListener("keyup", control);
+
+function moveDown() {
+  undraw();
+  currentPosition += width;
+  draw();
+  freeze();
+}
+
+function freeze() {
+  if (
+    current.some((index) =>
+      squares[currentPosition + index + width].classList.contains("taken")
+    )
+  ) {
+    current.forEach((index) =>
+      squares[currentPosition + index].classList.add("taken")
+    );
+    random = nextRandom;
+    nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+    current = theTetrominoes[random][currentRotation];
+    currentPosition = 4;
+    draw();
+    displayShape();
+    addScore();
+    gameOver();
+  }
+}
+
+function moveLeft() {
+  undraw();
+  const isAtLeftEdge = current.some(
+    (index) => (currentPosition + index) % width === 0
+  );
+  if (!isAtLeftEdge) currentPosition -= 1;
+  if (
+    current.some((index) =>
+      squares[currentPosition + index].classList.contains("taken")
+    )
+  ) {
+    currentPosition += 1;
+  }
+  draw();
+}
+
+function moveRight() {
+  undraw();
+  const isAtRightEdge = current.some(
+    (index) => (currentPosition + index) % width === width - 1
+  );
+  if (!isAtRightEdge) currentPosition += 1;
+  if (
+    current.some((index) =>
+      squares[currentPosition + index].classList.contains("taken")
+    )
+  ) {
+    currentPosition -= 1;
+  }
+  draw();
+}
+
+function rotate() {
+  undraw();
+  currentRotation++;
+  if (currentRotation === current.length) {
+    currentRotation = 0;
+  }
+  current = theTetrominoes[random][currentRotation];
+  draw();
+}
+
+const displaySquare = document.querySelectorAll(".mini-grid div");
+const displayWidth = 4;
+let displayIndex = 0;
+
+const upNextTetrominoes = [
+  [1, displayWidth + 1, displayWidth * 2 + 1, 2], //lTetromino
+  [0, displayWidth, displayWidth + 1, displayWidth * 2 + 1], //zTetromino
+  [1, displayWidth, displayWidth + 1, displayWidth + 2], //tTetromino
+  [0, 1, displayWidth, displayWidth + 1], //oTetromino
+  [1, displayWidth + 1, displayWidth * 2 + 1, displayWidth * 3 + 1] //iTetromino
+];
+
+function displayShape() {
+  displaySquare.forEach((square) => {
+    square.classList.remove("tetromino");
+    square.style.backgroundColor = "";
+  });
+
+  upNextTetrominoes[nextRandom].forEach((index) => {
+    displaySquare[displayIndex + index].classList.add("tetromino");
+    displaySquare[displayIndex + index].style.backgroundColor =
+      colors[nextRandom];
+  });
+}
+
+startBtn.addEventListener("click", () => {
+  //근데 버그는 있음 click할 때마다 displayShape에 블록이 달라지니까
+  //유저가 이를 악용할 소지가 있음!
+  if (timerId) {
+    clearInterval(timerId);
+    timerId = null;
+  } else {
+    draw();
+    timerId = setInterval(moveDown, 1000);
+    nextRandom = Math.floor(Math.random() * theTetrominoes.length);
+    displayShape();
+  }
+});
+
+function addScore() {
+  for (let i = 0; i < 199; i += width) {
+    const row = [
+      i,
+      i + 1,
+      i + 2,
+      i + 3,
+      i + 4,
+      i + 5,
+      i + 6,
+      i + 7,
+      i + 8,
+      i + 9
+    ];
+    if (row.every((index) => squares[index].classList.contains("taken"))) {
+      score += 10;
+      scoreDisplay.innerHTML = score;
+
+      row.forEach((index) => {
+        squares[index].classList.remove("taken");
+        squares[index].classList.remove("tetromino");
+        squares[index].style.backgroundColor = "";
+      });
+
+      // 요 부분이 좀 이해하기 어려운데...
+      // https://youtu.be/rAUn1Lom6dw?t=4867
+      const squaresRemoved = squares.splice(i, width);
+      // 잘라내고
+      //console.log(squaresRemoved);
+      squares = squaresRemoved.concat(squares);
+      squares.forEach((cell) => grid.appendChild(cell));
+      // 이어붙인다?
+    }
+  }
+}
+
+function gameOver() {
+  if (
+    current.some((index) =>
+      squares[currentPosition + index].classList.contains("taken")
+    )
+  ) {
+    scoreDisplay.innerHTML = "end";
+    clearInterval(timerId);
+  }
+}
